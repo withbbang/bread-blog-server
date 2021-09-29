bfe13def624940d4aad48fd1ad9dd34fe9841303
unknown <withbbang@gmail.com>
2021-09-29 17:07:06 +0900

    update annotation for parent param in resolver

--- resolvers/Type.js
+++ resolvers/Type.js
@@ -1,6 +1,8 @@
 const { GraphQLScalarType } = require('graphql');
 const metaData = require('../metaData');
 
+// parent 인자값은 커스텀 루트타입 쿼리만 가질 수 있다. 다른 기본 쿼리(Query, Mutation, Subscription)은 undefined
+// parent 인자값은 해당 함수를 품고 있는 객체를 가리킴. ex) postedBy 함수의 parent 인자값은 Photo을 가리킨다.
 module.exports = {
     Photo: {
         id: parent => parent.id || parent._id,
--- resolvers/index.js
+++ resolvers/index.js
@@ -7,7 +7,6 @@ const Type = require('./Type');
 // 0) 실제 데이터를 가공해서 보내주는 객체.
 // 1) 루트쿼리 Query, Mutation, Subscription 내부는 쿼리에 대한 반환값들을 함수 실행문으로 정의.
 // 2) 타입정의 내부 필드들은 특별히 따로 함수 실행문으로 리턴할 수 있다. ex) Photo 타입의 url 필드.
-// 3) parent 인자값은 해당 함수를 품고 있는 객체를 가리킴. ex) postPhoto 함수의 parent 인자값은 Mutation을 가리킨다.
 module.exports = {
   Query,
   Mutation,
